5e701c13d7bd6bbca707b3d27285559c739ab988
Thanaphorn Wutthikunprapan <t.thanaphorn.wutthikunprapan@scbtechx.io>
2022-08-18 17:29:33 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Profile from "./pages/profile/Profile";
+import Signin from "./pages/sign-in/Signin";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const token = localStorage.getItem("accessToken");
+  if (!token) {
+    return <Signin />;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} />
+        {/* <Signin /> */}
+      </Routes>
+      {/* <Profile /> */}
     </div>
   );
 }
---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MenuAppBar from "../../components/menu/MenuAppBar";
+
+export default function Profile() {
+  return (
+    <div>
+      <MenuAppBar />
+    </div>
+  );
+}
